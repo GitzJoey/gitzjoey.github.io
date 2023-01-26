d69b75a2ba2da11ccad884e375a0dd7d620db341
GitzJoey <gitzjoey@live.com>
2023-01-26 13:26:45 +0800

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-import "../scss/style.scss";
+import '../scss/style.scss';
+
+import $ from 'jquery';
+import * as bootstrap from 'bootstrap';
+import feather from 'feather-icons';
+
+window.$ = $;
+
+$(() => {
+    feather.replace();
+});
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 export default {
     root: process.cwd() + '/src',
     publicDir: process.cwd() + '/public',
+    resolve: {
+        alias: {
+            '~bootstrap': process.cwd() + '/node_modules/bootstrap',
+        }
+    },
     server: {
         port: 8080,
         hot: true
